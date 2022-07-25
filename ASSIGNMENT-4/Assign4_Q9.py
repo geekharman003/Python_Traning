5f545d0e533aada8e058883d2532dd76e294eea2
Harmanpreet Singh <109497413+geekharman003@users.noreply.github.com>
2022-07-25 23:34:24 +0530

    First commit

---
+++ ASSIGNMENT-4/Assign4_Q9.py
@@ -0,0 +1,14 @@
+# Example 9
+class Vehicle:
+ def __del__(self):
+  print("Vehicle object destroyed")
+print(10/0)
+v = Vehicle()
+del v
+# Output
+# Vehicle object destroyed
+# Exception ignored in: <function Vehicle.__del__ at 0x0000023AE69FA440>
+# Traceback (most recent call last):
+#   File "c:\Users\lenovo\Desktop\python traning\ASSIGNMENT-4\Q9.py", line 6, in  __del__
+#     print(10/0)
+# ZeroDivisionError: division by zero
