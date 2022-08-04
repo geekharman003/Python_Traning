799e4dc652d8a43b5f85a67e5b0ed2fcb5688da0
Harmanpreet Singh <109497413+geekharman003@users.noreply.github.com>
2022-08-04 10:35:13 +0530

    classwork added

---
+++ Day 3/dictionary.py
@@ -0,0 +1,6 @@
+mydict={"name":"harman",
+"roll no.":"2015048",
+"mydict2":{"mobile no":7814683871}
+}
+print(mydict["name"])
+print(mydict["mydict2"]["mobile no"])
---
+++ Day 3/functions.py
@@ -0,0 +1,4 @@
+def my_func(a,b):
+    c=a+b
+    return c
+print(my_func(10,20))
